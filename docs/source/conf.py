b3be84235c4a1b2ad6faae6407074454944ef1c8
Arian <88984301+Arian-Ott@users.noreply.github.com>
2023-11-03 11:37:43 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,4 +63,4 @@ language = 'en_uk'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
