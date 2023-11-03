603fa124b3e19d95f956e1a875cbcf71c16530d1
Arian <88984301+Arian-Ott@users.noreply.github.com>
2023-11-03 10:45:23 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = '0.1.2'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark',
+   
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode'
 ]
