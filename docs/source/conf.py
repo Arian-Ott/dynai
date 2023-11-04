b14160fb9d6ede7d344cfe67c360e4b4ed7ca724
Arian <88984301+Arian-Ott@users.noreply.github.com>
2023-11-05 00:04:55 +0100

    Changes
    Signed-off-by: Arian <88984301+Arian-Ott@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../dynai/'))
+sys.path.insert(0, os.path.abspath('../dynai/'))
 
 
 # -- Project information -----------------------------------------------------
@@ -30,9 +30,7 @@ release = '0.1.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
-
+extensions = ['sphinx.ext.autodoc']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
